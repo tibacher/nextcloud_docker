c442744b786103b046474c2033fee5dd2ae8a7ce
Tim Schaefer <tim.schaefer.de@gmail.com>
2021-06-07 15:20:58 +0200

    fixed monit maintanace

--- scripts/nextcloud_scripts/maintenance_disable.sh
+++ scripts/nextcloud_scripts/maintenance_disable.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker exec --user www-data nc_web php occ maintenance:mode --off
+sudo monit monitor nextcloud & sudo monit monitor nextcloud-local &
--- scripts/nextcloud_scripts/maintenance_enable.sh
+++ scripts/nextcloud_scripts/maintenance_enable.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+sudo monit unmonitor nextcloud
+sudo monit unmonitor nextcloud-local
+
 docker exec --user www-data nc_web  php occ maintenance:mode --on
