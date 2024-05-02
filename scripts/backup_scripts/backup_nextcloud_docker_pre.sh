c4948c0c3bcdaca4e3b9d16442a6a2d9c6293bd9
Tim Schäfer <tim.schaefer.de@gmail.com>
2024-05-02 16:48:11 +0200

    updated configs after npm manager rollout

--- scripts/backup_scripts/backup_nextcloud_docker_pre.sh
+++ scripts/backup_scripts/backup_nextcloud_docker_pre.sh
@@ -5,6 +5,7 @@ scripts_path=`realpath $(dirname $(readlink -f $0))/../`
 # enable maintanace mode
 bash $scripts_path/nextcloud_scripts/maintenance_enable.sh
 
-echo "Create Database Backup (Mysql Dump)"
+# echo "Create Database Backup (Mysql Dump)"
+
 # Create new sql dump backup
 docker exec nc_db bash -c 'mysqldump --single-transaction -u$(cat $MYSQL_USER_FILE) -p$(cat $MYSQL_PASSWORD_FILE) $(cat $MYSQL_DATABASE_FILE) > /var/lib/mysql/$(cat $MYSQL_DATABASE_FILE)-sqlbkp_`date +"%Y%m%d"`.bak'
